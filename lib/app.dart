9e6e81ff08229067ca7ab9ff149c4bb782afa898
Bakhtiar Arifin <arifin@tamasia.co.id>
2019-06-21 15:34:50 +0700

    application structure

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+import 'package:schedule_app/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
